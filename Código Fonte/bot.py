3b7c9929b8cba387449a8b272735abd083fa17ec
M1Z43L <33938612+mizaelarthur@users.noreply.github.com>
2024-08-14 22:46:55 +0000

    criando bot

--- Código Fonte/bot.py
+++ Código Fonte/bot.py
@@ -15,3 +15,25 @@ logado = False
 bot = telebot.TeleBot(CHAVE_API)
 
 
+def usuario(mensagem):
+    id_chat = mensagem.chat.id
+    credenciais[id_chat] = {'usuario':mensagem.text}
+    bot.send_message(id_chat, "INFORME A SENHA:")
+    bot.register_next_step_handler(mensagem, senha)
+
+
+def senha(mensagem):
+    global logado
+    id_chat = mensagem.chat.id
+    if id_chat in credenciais:
+        credenciais[id_chat]['senha'] = mensagem.text
+        dados_combinados = {'id_chat':id_chat,'credenciais':credenciais}
+        dados = json.dumps(dados_combinados)
+        socket_cliente.sendall(dados.encode())
+        logado = json.loads(socket_cliente.recv(4096).decode())
+        if logado:
+            bot.send_message(id_chat, f"SEJA BEM VINDO(A) {credenciais[id_chat]['usuario']}!")
+            menu(mensagem)
+            
+        else:
+            bot.send_message(id_chat, "USUÁRIO OU SENHA INCORRETOS, CLIQUE AQUI PARA INICIAR: /iniciar")

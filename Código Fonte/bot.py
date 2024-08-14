e11b3e8d069df819f8f4f2d337e589b709084d38
M1Z43L <33938612+mizaelarthur@users.noreply.github.com>
2024-08-14 22:44:02 +0000

    criando bot

--- Código Fonte/bot.py
+++ Código Fonte/bot.py
@@ -7,3 +7,11 @@ import json
 socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_cliente.connect(("localhost", 5000))
 
+# Aqui informamos a chave API do BOT, essa foi usada apenas como exemplo, a definitiva ainda será criada
+CHAVE_API = "7247756639:AAGqOKyr1Hl3OouOZWxXGae9Jz8_Z7fjnh0"
+credenciais = {}
+selecoes = []
+logado = False
+bot = telebot.TeleBot(CHAVE_API)
+
+

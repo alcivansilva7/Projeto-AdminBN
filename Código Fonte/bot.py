fc777948fc13fea6211974e0c95e10ed14ad5bfb
M1Z43L <33938612+mizaelarthur@users.noreply.github.com>
2024-08-15 00:31:19 +0000

    att

--- Código Fonte/bot.py
+++ Código Fonte/bot.py
@@ -8,7 +8,7 @@ socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_cliente.connect(("localhost", 5000))
 
 # Aqui informamos a chave API do BOT, essa foi usada apenas como exemplo, a definitiva ainda será criada
-CHAVE_API = "7247756639:AAGqOKyr1Hl3OouOZWxXGae9Jz8_Z7fjnh0"
+CHAVE_API = "Chave"
 credenciais = {}
 selecoes = []
 logado = False

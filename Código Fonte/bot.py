5bbad697b11e5c5fb68f0e68a26af989181b9581
Alcivan Silva <alcivansilva7@gmail.com>
2024-08-15 14:07:13 -0300

    Criando função listar ips no BOT

--- Código Fonte/bot.py
+++ Código Fonte/bot.py
@@ -3,8 +3,8 @@ import telebot, os
 import socket
 import json
 
-#socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#socket_cliente.connect(("localhost", 5000))
+socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_cliente.connect(("localhost", 5000))
 credenciais = {}
 selecoes = []
 logado = False
@@ -39,6 +39,25 @@ bot = telebot.TeleBot(os.getenv('BOT_CHAVE'))
 #            bot.send_message(id_chat, "USUÁRIO OU SENHA INCORRETOS, CLIQUE AQUI PARA INICIAR: /iniciar")
 
 #Criando Menu de funcionalidades iniciais do BOT
+
+@bot.message_handler(commands=["listar"])
+def listar_ips(mensagem):
+    id_chat = mensagem.chat.id
+    global logado
+    if logado:
+        dados = json.dumps("/listar")
+        socket_cliente.sendall(dados.encode())
+        retorno = json.loads(socket_cliente.recv(4096).decode())
+        if not retorno:
+            bot.send_message(id_chat, "PARECE QUE HOUVE UM ERRO")
+            menu(mensagem)
+        else:
+            saida = "\n".join(retorno)
+            bot.send_message(id_chat, saida)
+            menu(mensagem)
+    else:
+        bot.send_message(id_chat, "VOCÊ NÃO ESTÁ LOGADO! CLIQUE AQUI PARA INICIAR: /iniciar")
+
 def menu(mensagem):
     id_chat = mensagem.chat.id
     bot.send_message(id_chat, """ESCOLHA A OPÇÃO DESEJADA:
@@ -62,7 +81,7 @@ def default(mensagem):
 VOCÊ DEVE SE AUTENTICAR PRIMEIRO!""")
     #bot.send_message(id_chat, "DIGITE ALGO")
     #bot.register_next_step_handler(mensagem, echo)
-    menu(mensagem)
+
     
 
 # chama o objeto BOT para ficar em execução, é responsável por verificar continuamente se o bot está recebendo novas mensagens
